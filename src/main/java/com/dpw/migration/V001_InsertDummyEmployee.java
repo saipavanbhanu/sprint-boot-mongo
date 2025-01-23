180837867e711d53007c509f0551204fb15e071c
Bhanu Sai Pavan Kumar Pothuri <bhanu.pothuri@dpworld.com>
2025-01-23 13:23:53 +0530

    mongo ck migration enabled

---
+++ src/main/java/com/dpw/migration/V001_InsertDummyEmployee.java
@@ -0,0 +1,36 @@
+package com.dpw.migration;
+
+import com.dpw.entity.Employee;
+import com.dpw.repository.EmployeeRepository;
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@ChangeUnit(id="inserting-employee", order = "001", author = "mongock")
+@Slf4j
+@AllArgsConstructor
+public class V001_InsertDummyEmployee {
+
+	private final EmployeeRepository employeeRepository;
+
+	@Execution
+	public void migrationMethod() {
+		log.info("before inserting employee.");
+		Employee employee = new Employee();
+		employee.setId("id01");
+		employee.setName("RandomPerson1");
+		employee.setDepartment("RandomDepartment1");
+		employee.setSalary(1000);
+		employeeRepository.save(employee);
+	}
+	@RollbackExecution
+	public void rollback() {
+		employeeRepository.deleteById("id01");
+	}
+
+
+}
--- src/main/java/com/dpw/repository/EmployeeRepository.java
+++ src/main/java/com/dpw/repository/EmployeeRepository.java
@@ -2,6 +2,9 @@ package com.dpw.repository;
 
 import com.dpw.entity.Employee;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository("employees")
 public interface EmployeeRepository extends MongoRepository<Employee, String> {
+
 }
